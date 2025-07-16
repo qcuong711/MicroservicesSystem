ba78a59622937c9f0d3736e9f3aa6ac466ba8353
qcuong711 <dangquangcuong711@gmail.com>
2025-07-16 09:04:20 +0700

    add

--- DataManagementApi/obj/Debug/net8.0/DataManagementApi.AssemblyInfo.cs
+++ DataManagementApi/obj/Debug/net8.0/DataManagementApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataManagementApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7559d29a676a34a56a20aaac741ac01e91ae9e06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c62e7dcb1f7095f88a4283645bfef82f76853475")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataManagementApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataManagementApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
